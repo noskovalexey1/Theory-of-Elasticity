8fe6ec8702407784d3acdbb530bcdf026f59cc10
Maxim Kirichenko <kiri4max@yandex.ru>
2025-01-20 12:42:58 +0300

    Create plot_trajectory.py

---
+++ managers/plot_trajectory.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+def plot_trajectory(mb, tr):
+    for i in range(len(mb.material_points)):
+        for j in range(len(mb.material_points)):
+            plt.plot(mb.material_points[i].coord_x, mb.material_points[j].coord_y, 'r.')
+            print(i)
+    for i in range(len(mb.material_points)):
+        for j in range(len(mb.material_points)):
+            plt.plot(tr.point_trajectories[i].x, tr.point_trajectories[j].y, 'b', linewidth=0.3)
+            time = len(tr.point_trajectories[i].x) - 1
+            plt.plot(tr.point_trajectories[i].x[time], tr.point_trajectories[j].y[time], 'g.')
+    plt.axis('equal')
+    plt.grid()
+    plt.show()
